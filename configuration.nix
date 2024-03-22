02882ad1383fba8fd452822d1aca557acd553b3e
= <=>
2024-03-22 20:45:03 +1100

    floorp

--- configuration.nix
+++ configuration.nix
@@ -130,11 +130,11 @@
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      neovim
      wget
-     firefox
      networkmanagerapplet
      nerdfonts
      font-awesome
      mpd
+     floorp
 
      # Terminal
      alacritty
