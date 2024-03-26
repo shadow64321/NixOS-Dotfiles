260cc643a594690b10d877f55048f444c12512f2
= <=>
2024-03-26 22:56:26 +1100

    Audio Visualizer

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports =
@@ -138,6 +138,7 @@
      anki
      vlc
      obs-studio
+     cli-visualizer
      
      # Gaming
      steam
@@ -161,6 +162,20 @@
      rofi-wayland
      pyprland
   ];
+
+  # Stuff to make steam work
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+    "steam-original"
+    "steam-run"
+  ];
+
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
